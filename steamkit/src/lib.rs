00a8d6f3f46241dd40ce6612c683f421919414c2
JAD3N <me@jaden.bio>
2023-05-14 20:58:10 +0100

    Fixing Bugs

--- steamkit/src/lib.rs
+++ steamkit/src/lib.rs
@@ -1,21 +1,3 @@
-use client::Client;
-use connection::Connection;
-use futures::channel::mpsc;
-
-#[macro_use]
-extern crate lazy_static;
-
-pub mod client;
-pub mod connection;
-pub mod id;
-pub mod request;
-
+pub use steamkit_client as client;
+pub use steamkit_protos as protos;
 pub use steamkit_vdf as vdf;
-
-pub fn connect() -> (Client, Connection) {
-    let (sender, receiver) = mpsc::unbounded();
-    let client = Client::new(sender);
-    let connection = Connection::new(receiver);
-
-    (client, connection)
-}
