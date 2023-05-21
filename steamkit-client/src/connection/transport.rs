6a5c30e28612d395b4c12b5cde685e756e75bdee
jaden <me@jaden.bio>
2023-05-21 20:51:57 +0000

    WIP Connection Stuff

--- steamkit-client/src/connection/transport.rs
+++ steamkit-client/src/connection/transport.rs
@@ -1,7 +1,61 @@
-pub struct TcpTransport {}
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf, ReadHalf, WriteHalf},
+        TcpStream, ToSocketAddrs,
+    },
+};
+
+pub struct TcpTransport {
+    rx: OwnedReadHalf,
+    tx: OwnedWriteHalf,
+}
 
 impl TcpTransport {
-    
+    pub const MAGIC: &str = "VT01";
+
+    pub async fn connect<A: ToSocketAddrs>(addr: A) -> anyhow::Result<Self> {
+        let stream = TcpStream::connect(addr).await?;
+        let (rx, tx) = stream.into_split();
+        Ok(Self { rx, tx })
+    }
+
+    async fn read(rx: &mut OwnedReadHalf, session_key: Option<Vec<u8>>) -> anyhow::Result<Vec<u8>> {
+        // read message length
+        let len = rx.read_u32_le().await?;
+
+        // read header magic
+        let mut header = vec![0; 4];
+        rx.read_exact(&mut header).await?;
+
+        // decode magic from bytes
+        let magic = String::from_utf8(header)?;
+        if magic != Self::MAGIC {
+            anyhow::bail!("Connection out of sync");
+        }
+
+        let mut data = vec![0; len as usize];
+        rx.read_exact(&mut data).await?;
+
+        // TODO: decrypt
+
+        Ok(data)
+    }
+
+    async fn write(tx: &mut OwnedWriteHalf, data: &[u8]) -> anyhow::Result<()> {
+        // TODO: encypt data before doing anything
+
+        // get length as u32 le bytes
+        let len = data.len();
+
+        tx.write_u32_le(len as u32).await?;
+        tx.write_all(Self::MAGIC.as_bytes()).await?;
+        tx.write_all(data).await?;
+
+        Ok(())
+    }
+
+    // async fn wr
 }
 
 // pub struct WsTransport {
@@ -11,4 +65,16 @@ impl TcpTransport {
 pub enum Transport {
     Tcp(TcpTransport),
     // Ws(WsTransport),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TcpTransport;
+
+    #[tokio::test]
+    async fn try_connect() {
+        TcpTransport::connect("162.254.193.102:27017")
+            .await
+            .unwrap();
+    }
 }
---
+++ steamkit-util/src/crypto.rs
@@ -0,0 +1,74 @@
+use lazy_static::lazy_static;
+use rand::Rng;
+use rsa::{pkcs8::DecodePublicKey, Oaep, RsaPublicKey};
+use sha1::Sha1;
+
+lazy_static! {
+    static ref PUBLIC_KEY: RsaPublicKey =
+        DecodePublicKey::from_public_key_pem(include_str!("system.pem")).unwrap();
+}
+
+#[derive(Debug, Clone)]
+pub struct SessionKey {
+    pub plain: Vec<u8>,
+    pub encrypted: Vec<u8>,
+}
+
+impl SessionKey {
+    pub fn generate(nonce: Option<Vec<u8>>) -> Self {
+        let mut rng = rand::thread_rng();
+
+        let session_key = rng.gen::<[u8; 32]>().to_vec();
+        let nonce = nonce.unwrap_or_default();
+
+        let mut buf = session_key.clone();
+        buf.extend_from_slice(&nonce);
+
+        let padding = Oaep::new::<Sha1>();
+        let encrypted = PUBLIC_KEY
+            .encrypt(&mut rng, padding, &buf)
+            .expect("failed to encrypt");
+
+        Self {
+            plain: session_key,
+            encrypted,
+        }
+    }
+}
+
+pub fn symmetric_decrypt(data: &[u8], key: &[u8], check_hmac: bool) {
+    
+}
+
+pub mod hash {
+    use crc::{Crc, CRC_32_ISO_HDLC};
+    use sha1::{Digest, Sha1};
+
+    pub fn sha1(data: &[u8]) -> Vec<u8> {
+        let mut sha = Sha1::new();
+        sha.update(data);
+        sha.finalize().to_vec()
+    }
+
+    pub fn crc32(data: &[u8]) -> u32 {
+        let crc = Crc::<u32>::new(&CRC_32_ISO_HDLC);
+        crc.checksum(data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hash;
+
+    #[test]
+    fn sha1() {
+        let value = hash::sha1(b"the quick brown fox jumps over the lazy dog");
+        assert_eq!(hex::encode(value), "16312751ef9307c3fd1afbcb993cdc80464ba0f1");
+    }
+
+    #[test]
+    fn crc32() {
+        let value = hash::crc32(b"the quick brown fox jumps over the lazy dog");
+        assert_eq!(value, 3456913684);
+    }
+}
