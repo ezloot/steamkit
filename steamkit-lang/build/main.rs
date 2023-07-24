cf33ae65fd61b03f477eb17dbc1e2a2686e8a580
JAD3N <me@jaden.bio>
2023-07-24 20:45:22 +0100

    Identify Requirements

--- steamkit-lang/build/main.rs
+++ steamkit-lang/build/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+// TODO: support classes
+// TODO: support enums
+// TODO: support flags
+// TODO: add a way to map EMsg to message classes
+// TODO: support class constants (as pub const on struct impls)
+// TODO: `use derive-new` to add default values for new
+// TODO: show removed enum variants as commented out (with message if available)
+// TODO: show obsolete enum variants as deprecated (with message if available)
+
 fn main() {
-    todo!()
+    let cwd = env::current_dir().unwrap();
+    eprintln!("{:?}", cwd);
+    // panic!(cwd);
+    panic!();
 }
