3e3700cd43c6dde870bd315c410a16735efeb97f
JAD3N <me@jaden.bio>
2023-08-20 23:58:54 +0100

    Simplify Parser

--- steamkit-vpk/src/lib.rs
+++ steamkit-vpk/src/lib.rs
@@ -1,10 +1,12 @@
 mod error;
 
+use std::collections::HashMap;
+
 pub use error::*;
 use nom::{
     bytes::complete::{tag, take, take_until},
     character::complete::char,
-    combinator::{map, map_res, recognize},
+    combinator::{map, map_res, peek, recognize},
     multi::many_till,
     number::complete::{le_u32, le_u8},
     sequence::{pair, tuple},
@@ -18,16 +20,23 @@ impl Vpk {
     pub const SIGNATURE: u32 = 0x55aa1234;
 }
 
-#[derive(Debug)]
-pub enum Header {
-    V1 {
-        signature: u32,
-        version: u32,
-        tree_length: u32,
-    },
+#[derive(Debug, Nom)]
+#[nom(LittleEndian)]
+pub struct Header {
+    pub signature: u32,
+    pub version: u32,
+    #[nom(Parse = "{ |i| HeaderData::parse(i, version) }")]
+    pub data: HeaderData,
+}
+
+#[derive(Debug, Nom)]
+#[nom(Selector = "u32")]
+#[nom(LittleEndian)]
+pub enum HeaderData {
+    #[nom(Selector = "1")]
+    V1 { tree_length: u32 },
+    #[nom(Selector = "2")]
     V2 {
-        signature: u32,
-        version: u32,
         tree_length: u32,
         data_length: u32,
         md5_length: u32,
@@ -36,18 +45,12 @@ pub enum Header {
     },
 }
 
-impl Header {
+impl HeaderData {
     pub fn tree_length(&self) -> u32 {
-        let test = &[0u8];
         match self {
             Self::V2 { tree_length, .. } | Self::V1 { tree_length, .. } => *tree_length,
         }
     }
-}
-
-impl Header {
-    pub const V1_LENGTH: usize = 4 * 3;
-    pub const V2_LENGTH: usize = 4 * 7;
 }
 
 #[derive(Debug, Nom)]
@@ -63,52 +66,27 @@ pub struct DirectoryEntry {
     pub payload: Vec<u8>,
 }
 
-pub fn header(input: &[u8]) -> IResult<&[u8], Header, Error<&[u8]>> {
-    let (input, (signature, version, tree_length)) = tuple((le_u32, le_u32, le_u32))(input)?;
-
-    // check vpk signature is valid
-    if signature != Vpk::SIGNATURE {
-        return Err(nom::Err::Error(Error::InvalidSignature(signature)));
-    }
-
-    match version {
-        1 => Ok((
-            input,
-            Header::V1 {
-                signature,
-                version,
-                tree_length,
-            },
-        )),
-        2 => map(
-            tuple((le_u32, le_u32, le_u32, le_u32)),
-            |(data_length, md5_length, external_md5_length, signature_length)| Header::V2 {
-                signature,
-                version,
-                tree_length,
-                data_length,
-                md5_length,
-                external_md5_length,
-                signature_length,
-            },
-        )(input),
-        _ => Err(nom::Err::Error(Error::UnsupportedVersion(version))),
-    }
+#[derive(Debug)]
+pub struct Entry {
+    pub path: String,
+    pub dir_entry: DirectoryEntry,
 }
 
-pub fn cstring(input: &[u8]) -> IResult<&[u8], String, Error<&[u8]>> {
+pub fn cstring(input: &[u8]) -> IResult<&[u8], String> {
     let (input, (bytes, _)) = many_till(le_u8, tag(&[0]))(input)?;
 
     match String::from_utf8(bytes) {
         Ok(s) => Ok((input, s)),
-        Err(_) => Err(nom::Err::Error(Error::Parse(nom::error::Error {
+        Err(_) => Err(nom::Err::Error(nom::error::Error {
             input,
             code: nom::error::ErrorKind::IsNot,
-        }))),
+        })),
     }
 }
 
-pub fn tree(mut input: &[u8]) -> IResult<&[u8], (), Error<&[u8]>> {
+pub fn tree(mut input: &[u8]) -> IResult<&[u8], HashMap<String, Entry>> {
+    let mut map = HashMap::new();
+
     loop {
         let (i, ext) = cstring(input)?;
         input = i;
@@ -144,15 +122,13 @@ pub fn tree(mut input: &[u8]) -> IResult<&[u8], (), Error<&[u8]>> {
                     path = format!("{dir}/{path}");
                 }
 
-                let (i, entry) =
-                    DirectoryEntry::parse(input).map_err(|e| e.map(|e| Error::Parse(e)))?;
+                let (i, dir_entry) = DirectoryEntry::parse(input)?;
                 input = i;
 
-                // println!("{path} {entry:?}");
-                // break;
+                map.insert(path.clone(), Entry { path, dir_entry });
             }
         }
     }
 
-    Ok((input, ()))
+    Ok((input, map))
 }
--- steamkit-vpk/src/main.rs
+++ steamkit-vpk/src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs::File, io::Read};
 
+use nom_derive::Parse;
 use steamkit_vpk::*;
 
 fn main() {
@@ -7,12 +8,16 @@ fn main() {
     let mut file = File::open("assets/pak01_dir.vpk").unwrap();
     file.read_exact(&mut buf).unwrap();
 
-    let (_, head) = header(&buf).unwrap();
+    // let (_, head) = header(&buf).unwrap();
 
-    let mut buf = vec![0; head.tree_length() as usize];
+    let (_, header) = Header::parse(&buf).unwrap();
+
+    let mut buf = vec![0; header.data.tree_length() as usize];
     file.read_exact(&mut buf).unwrap();
 
-    let (_, s) = tree(&buf).unwrap();
+    let (_, tree) = tree(&buf).unwrap();
+
+    println!("{}", tree.len());
 
     // println!("{:?}", s);
 }
