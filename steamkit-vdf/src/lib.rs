28e6b4d0c4c5807e4aae3368ee36545d53f6ceb5
JAD3N <me@jaden.bio>
2023-08-28 15:39:58 +0100

    WIP

--- steamkit-vdf/src/lib.rs
+++ steamkit-vdf/src/lib.rs
@@ -1,31 +1,80 @@
 mod error;
-mod node;
-mod token;
 
-use std::convert::TryFrom;
+use std::ops;
 
 pub use error::*;
-pub use node::*;
-pub use token::*;
+use indexmap::IndexMap;
+use nom::{
+    branch::alt,
+    bytes::complete::{escaped, is_not},
+    character::complete::{alphanumeric1, char, none_of, not_line_ending, one_of, space0, space1},
+    combinator::{cut, recognize},
+    error::{context, ParseError},
+    multi::many1,
+    sequence::{pair, preceded, terminated},
+    IResult,
+};
 
-#[derive(Debug, Clone, Default)]
-pub struct Vdf {
-    tokens: Tokens,
-    root: Nodes,
+pub struct KeyValue {
+    pub key: String,
+    pub value: Value,
+    pub macro_: Option<String>,
 }
 
-impl Vdf {
-    pub fn root(&self) -> &Nodes {
-        &self.root
-    }
+pub enum Value {
+    String(String),
+    Map(IndexMap<String, KeyValue>),
+}
 
-    pub fn tokens(&self) -> &Tokens {
-        &self.tokens
-    }
+// impl ops::Add for KeyValue {
+//     type Output = Self;
 
-    pub fn parse<S: Into<String>>(s: S) -> Result<Self> {
-        let tokens = Tokens::parse(s)?;
-        let root = Nodes::try_from(&tokens)?;
-        Ok(Self { tokens, root })
+//     fn add(self, rhs: Self) -> Self::Output {
+
+//     }
+// }
+
+fn comment(input: &str) -> IResult<&str, &str> {
+    preceded(char('/'), not_line_ending)(input)
+}
+
+fn unquoted_string(input: &str) -> IResult<&str, &str> {
+    context("unquoted_string", recognize(many1(none_of("\"{}\n\r\t "))))(input)
+}
+
+fn quoted_string(input: &str) -> IResult<&str, &str> {
+    context(
+        "quoted_string",
+        preceded(
+            char('"'),
+            cut(terminated(
+                escaped(none_of("\"\\"), '\\', one_of("\"nt\\")),
+                char('"'),
+            )),
+        ),
+    )(input)
+}
+
+fn string(input: &str) -> IResult<&str, &str> {
+    context("string", alt((quoted_string, unquoted_string)))(input)
+}
+
+fn key_value(input: &str) -> IResult<&str, (&str, &str)> {
+    pair(preceded(space0, string), preceded(space1, string))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{fs::File, io::Read};
+
+    use crate::*;
+
+    #[test]
+    fn test_me() {
+        let mut file = File::open("assets/items_game.txt").unwrap();
+        let mut data = String::new();
+        file.read_to_string(&mut data).unwrap();
+
+        println!("{:?}", key_value("\"Hello World\"    \"\\\"World :D:D:D\"").unwrap().1);
     }
 }
