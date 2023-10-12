ea7c6e2781f341cc8caf4e51ab0df1a0ba676ccb
JAD3N <me@jaden.bio>
2023-10-12 20:55:21 +0100

    Add Flat & Nested Key Values

--- steamkit-kv/src/error.rs
+++ steamkit-kv/src/error.rs
@@ -2,21 +2,8 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    // #[error("unexpected end of file")]
-    // EndOfFile,
-    // #[error("bad escape sequence: `{0}`")]
-    // BadEscape(char),
-    // #[error("unclosed quote string")]
-    // UnclosedQuote,
-    // #[error("unclosed quote string")]
-    // UnclosedCondition,
-    // #[error("syntax error caused by: `{0}`")]
-    // Syntax(char),
-    // #[error("unexpected token: {0:?}")]
-    // UnexpectedToken(Token),
-    // #[error("cannot perform lookup on value nodes")]
-    // Lookup,
-    // #[cfg(feature = "serde")]
-    // #[error("cannot serialize node: `{0}`")]
-    // Deserialize(String),
+    #[error("Failed to parse input")]
+    Parse,
+    #[error("Unexpected input after parsing: {0}")]
+    UnexpectedInput(String),
 }
--- steamkit-kv/src/lib.rs
+++ steamkit-kv/src/lib.rs
@@ -4,25 +4,158 @@ mod parser;
 use std::collections::HashMap;
 
 pub use error::*;
-use parser::*;
+use indexmap::IndexMap;
 
 pub type Path = Vec<String>;
-pub struct KeyValue(HashMap<Path, String>);
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum KeyValue {
+    String(String),
+    Map(IndexMap<String, Self>),
+}
+
+impl From<String> for KeyValue {
+    fn from(value: String) -> Self {
+        Self::String(value)
+    }
+}
+
+impl From<&str> for KeyValue {
+    fn from(value: &str) -> Self {
+        Self::String(value.to_string())
+    }
+}
+
+fn merge(entries: Vec<parser::Entry>) -> IndexMap<String, KeyValue> {
+    let mut map = IndexMap::new();
+
+    for entry in entries {
+        let kv = match entry.value {
+            parser::Value::String(s) => KeyValue::String(s),
+            parser::Value::Map(v) => KeyValue::Map(merge(v)),
+        };
+
+        // no need to check collisions if it doesn't exist
+        if !map.contains_key(&entry.key) {
+            map.insert(entry.key, kv);
+            continue;
+        }
+
+        // don't need to check if existing entry is a map because we're replacing it with a string
+        if let KeyValue::String(_) = kv {
+            map.insert(entry.key, kv);
+            continue;
+        }
+
+        // if we're here, we know that the existing entry is a map and we're trying to insert a map
+        match map.get_mut(&entry.key).unwrap() {
+            KeyValue::Map(map) => {
+                if let KeyValue::Map(value_map) = kv {
+                    map.extend(value_map);
+                }
+            }
+            KeyValue::String(_) => {
+                map.insert(entry.key, kv);
+            }
+        }
+    }
+
+    map
+}
 
 impl KeyValue {
     pub fn parse(input: &str) -> Result<Self> {
-        let mut map = HashMap::new();
-        let (_input, entry) = key_value(input).unwrap();
+        let (input, entry) = parser::key_value(input.trim()).map_err(|_| Error::Parse)?;
 
-        fn process(map: &mut HashMap<Path, String>, entry: Entry, path: &Path) {
+        // check if input is empty
+        if !input.is_empty() {
+            return Err(Error::UnexpectedInput(input.to_string()));
+        }
+
+        Ok(Self::Map(merge(vec![entry])))
+    }
+
+    pub fn get<I, T>(&self, path: I) -> Option<&Self>
+    where
+        I: IntoIterator<Item = T>,
+        T: AsRef<str>,
+    {
+        let mut iter = path.into_iter().peekable();
+        let path = iter.next();
+
+        if let Some(path) = path {
+            let path = path.as_ref();
+            match self {
+                Self::String(_) => None,
+                Self::Map(map) => match map.get(path) {
+                    Some(kv) => {
+                        if iter.peek().is_some() {
+                            kv.get(iter.collect::<Vec<_>>())
+                        } else {
+                            Some(kv)
+                        }
+                    }
+                    None => None,
+                },
+            }
+        } else {
+            Some(self)
+        }
+    }
+
+    pub fn get_mut<I, T>(&mut self, path: I) -> Option<&mut Self>
+    where
+        I: IntoIterator<Item = T>,
+        T: AsRef<str>,
+    {
+        let mut iter = path.into_iter().peekable();
+        let path = iter.next();
+
+        if let Some(path) = path {
+            let path = path.as_ref();
+            match self {
+                Self::String(_) => None,
+                Self::Map(map) => match map.get_mut(path) {
+                    Some(kv) => {
+                        if iter.peek().is_some() {
+                            kv.get_mut(iter.collect::<Vec<_>>())
+                        } else {
+                            Some(kv)
+                        }
+                    }
+                    None => None,
+                },
+            }
+        } else {
+            Some(self)
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct FlatKeyValues {
+    map: HashMap<Path, String>,
+}
+
+impl FlatKeyValues {
+    pub fn parse(input: &str) -> Result<Self> {
+        let mut map = HashMap::new();
+        let (input, entry) = parser::key_value(input.trim()).map_err(|_| Error::Parse)?;
+
+        // check if input is empty
+        if !input.is_empty() {
+            return Err(Error::UnexpectedInput(input.to_string()));
+        }
+
+        fn process(map: &mut HashMap<Path, String>, entry: parser::Entry, path: &Path) {
             let mut path = path.clone();
             path.push(entry.key.clone());
 
             match entry.value {
-                Value::String(s) => {
+                parser::Value::String(s) => {
                     map.insert(path, s);
                 }
-                Value::Map(entries) => {
+                parser::Value::Map(entries) => {
                     for entry in entries {
                         process(map, entry, &path);
                     }
@@ -32,10 +165,85 @@ impl KeyValue {
 
         process(&mut map, entry, &vec![]);
 
-        Ok(Self(map))
+        Ok(Self { map })
     }
 
-    pub fn get(&self, path: &Path) -> Option<&String> {
-        self.0.get(path)
+    pub fn get<I, T>(&self, path: I) -> Option<&String>
+    where
+        I: IntoIterator<Item = T>,
+        T: AsRef<str>,
+    {
+        let path = path
+            .into_iter()
+            .map(|s| s.as_ref().to_string())
+            .collect::<Vec<_>>();
+        self.map.get(&path)
+    }
+
+    pub fn get_str<I, T>(&self, path: I) -> Option<&str>
+    where
+        I: IntoIterator<Item = T>,
+        T: AsRef<str>,
+    {
+        self.get(path).map(|s| s.as_str())
+    }
+
+    pub fn get_mut<I, T>(&mut self, path: I) -> Option<&mut String>
+    where
+        I: IntoIterator<Item = T>,
+        T: AsRef<str>,
+    {
+        let path = path
+            .into_iter()
+            .map(|s| s.as_ref().to_string())
+            .collect::<Vec<_>>();
+        self.map.get_mut(&path)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = r#"
+            "key1"
+            {
+                "key2" "value1"
+                "key3"
+                {
+                    "key4" "value2"
+                }
+            }
+        "#;
+
+        let kv = KeyValue::parse(input).unwrap();
+        assert_eq!(
+            kv.get(&["key1", "key2"]),
+            Some(&KeyValue::String("value1".into()))
+        );
+        assert_eq!(
+            kv.get(&["key1", "key3", "key4"]),
+            Some(&KeyValue::String("value2".into()))
+        );
+    }
+
+    #[test]
+    fn test_parse_flat() {
+        let input = r#"
+            "key1"
+            {
+                "key2" "value1"
+                "key3"
+                {
+                    "key4" "value2"
+                }
+            }
+        "#;
+
+        let kv = FlatKeyValue::parse(input).unwrap();
+        assert_eq!(kv.get_str(&["key1", "key2"]), Some("value1"));
+        assert_eq!(kv.get_str(&["key1", "key3", "key4"]), Some("value2"));
     }
 }
